e7b3fc8f6f62384533d96f5fb8d11bd3f8070c2a
Pavel <mouseoskarx7@gmail.com>
2021-06-19 01:58:17 +0300

    add testing environment for exploring TDD method

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-
+#include "test.h"
 int main()
 {
+//while ci/cd system is running, this is executed
+#ifdef TEST_SESSION
+    test(); //run tests
+#endif
+//otherwise, the real code is running
+#ifdef BUILD_SESSION
     std::cout << "Hello Classes With counters" << std::endl;
+#endif
 }
